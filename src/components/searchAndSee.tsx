c07c0d2b1971c63cb84d1b2a0795ad2859c07001
brunoocastro <bruno.scastro2012@hotmail.com>
2022-04-24 02:31:45 -0300

    feat: Atualizando texto no input ao alterar

--- src/components/searchAndSee.tsx
+++ src/components/searchAndSee.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const SearchAndSee = ({ code }: { code: string }) => {
@@ -7,26 +7,28 @@ const SearchAndSee = ({ code }: { code: string }) => {
 
   const [localCode, setLocalCode] = useState(code ?? "");
 
+  useEffect(() => {
+    code !== localCode && setLocalCode(code);
+  }, [code, localCode]);
+
   return (
     <div>
       <h1 className="text-32 font-bold mb-[10px]">- Procuraí</h1>
       <h2 className="text-20 text-ui-blue">Rastreador de encomendas</h2>
-        <div className="relative flex items-center justify-center mt-16 w-[24.188rem] pb-[10px] border-b-2 border-ui-primary">
-          <input
-            className="outline-none w-full text-left bg-transparent placeholder:text-ui-grayLight "
-            placeholder="Digite um código de rastreio"
-            value={localCode}
-            onChange={(e) => {
-              router.push(`/${e.target.value.toUpperCase()}`, undefined, {
-                shallow: true,
-              });
-              setLocalCode(e.target.value.toUpperCase());
-            }}
-          />
-          <FaSearch
-            className="mx-2 cursor-pointer"
-          />
-        </div>
+      <div className="relative flex items-center justify-center mt-16 w-[24.188rem] pb-[10px] border-b-2 border-ui-primary">
+        <input
+          className="outline-none w-full text-left bg-transparent placeholder:text-ui-grayLight "
+          placeholder="Digite um código de rastreio"
+          value={localCode}
+          onChange={(e) => {
+            router.push(`/${e.target.value.toUpperCase()}`, undefined, {
+              shallow: true,
+            });
+            setLocalCode(e.target.value.toUpperCase());
+          }}
+        />
+        <FaSearch className="mx-2 cursor-pointer" />
+      </div>
     </div>
   );
 };
